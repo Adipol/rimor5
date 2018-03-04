5be202ed563ef3fa5c72e015a844b5c5f94fc867
George <adipol13@gmail.com>
2018-03-04 15:17:57 -0400

    cap9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,18 +36,23 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+					<li><a href="{{route( 'blog' )}}">Blog</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @guest
+                        @guest  
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+						@else 
+							<li><a href="{{route('tags.index')}}">Etiquetas</a></li>
+							<li><a href="{{route('categories.index')}}">Categorias</a></li>
+                            <li><a href="{{route('posts.index')}}">Entradas</a></li>
+                            
+							<li class="dropdown">
+							 
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 

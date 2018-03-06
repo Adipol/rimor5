734fd9186e8310bcb714f88eb373edd4c74c9e03
George <adipol13@gmail.com>
2018-03-06 15:28:18 -0400

    cap14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,9 +75,36 @@
                 </div>
             </div>
         </nav>
+		@if(session('info'))
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="alert alert-success">
+							{{session('info')}}
+						</div>
+					</div>
+				</div>
+			</div>
+		@endif
+
+	@if(count($errors))
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="alert alert-danger">
+						<ul>
+							@foreach($errors->all() as $error)
+						      <li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	@endif
 
         @yield('content')
-    </div>
+    </div> 
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>

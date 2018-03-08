00ffde0fa09f61e094bc110a3217ef124431e44c
George <adipol13@gmail.com>
2018-03-07 21:07:24 -0400

    cap21

--- app/Http/Requests/PostStoreRequest.php
+++ app/Http/Requests/PostStoreRequest.php
@@ -23,9 +23,19 @@ class PostStoreRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        $rules= [
 			'name'=>'required',
-			'slug'=>'required|unique:categories,slug',
+			'slug'=>'required|unique:posts,slug',
+			'user_id'=>'required|integer',
+			'category_id'=>'required|integer',
+			'tags'=>'required|array',
+			'body'=>'required',
+			'status'=>'required|in:DRAFT,PUBLISHED'
 		];
+
+ 		if($this->get('file'))
+		$rules = array_mergue($rules,['file'=>'mimes:jpg,jpeg,png']);
+
+ 		return $rules;
     }
 }
